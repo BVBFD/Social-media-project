1a4b63e080ee36a2a288a301bd6177de702d53fd
BVBFD <lsevina126@gmail.com>
2022-03-11 18:16:05 +0900

    Code login and sign up system on the server part

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,18 +1,45 @@
 import express from 'express';
 import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
 
 const router = express.Router();
 
 // Register
-router.get('/register', async (req, res, next) => {
-  const user = await new User({
-    username: 'Lee Seong Eun',
-    email: 'lsevina126@gmail.com',
-    password: '123456',
-  });
+router.post('/register', async (req, res, next) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-  await user.save();
-  res.send('OK!');
+    const newUser = await new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Login
+router.post('/login', async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).send('user not found');
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(401).json('wrong password');
+
+    const { password, ...userInfo } = user._doc;
+    res.status(200).json(userInfo);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 export default router;
