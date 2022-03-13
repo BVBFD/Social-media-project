86f5c7ce3a67a8bf03b753d58393279fa74bdd50
BVBFD <lsevina126@gmail.com>
2022-03-13 12:05:00 +0900

    Code topbar on client side

---
+++ client/src/components/topbar/Topbar.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import styles from './Topbar.module.css';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+const Topbar = (props) => {
+  return (
+    <div className={styles.topbarContainer}>
+      <div className={styles.topbarLeft}>
+        <span className={styles.logo}>Mr.Lee Social</span>
+      </div>
+      <div className={styles.topbarCenter}>
+        <div className={styles.searchBar}>
+          <SearchIcon className={styles.searchIcon} />
+          <input
+            placeholder='Search for friend, post or video'
+            className={styles.searchInput}
+          />
+        </div>
+      </div>
+      <div className={styles.topbarRight}>
+        <div className={styles.topbarLinks}>
+          <span className={styles.topbarLink}>Homepage</span>
+          <span className={styles.topbarLink}>Timeline</span>
+        </div>
+        <div className={styles.topbarIcons}>
+          <div className={styles.topbarIconItem}>
+            <PersonIcon />
+            <span className={styles.topbarIconBadge}>1</span>
+          </div>
+
+          <div className={styles.topbarIconItem}>
+            <ChatIcon />
+            <span className={styles.topbarIconBadge}>2</span>
+          </div>
+
+          <div className={styles.topbarIconItem}>
+            <NotificationsIcon />
+            <span className={styles.topbarIconBadge}>1</span>
+          </div>
+        </div>
+        <img className={styles.topbarImg} src='../assets/person/1.jpeg' />
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
